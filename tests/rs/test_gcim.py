fe5f7598e36d1cfdf92ebb3b8a6ec26594985be8
Davit Shahnazaryan <davit.shahnazaryan@iusspavia.it>
2026-05-04 12:14:23 +0200

    fix: gcim tests

--- tests/rs/test_gcim.py
+++ tests/rs/test_gcim.py
@@ -1,20 +1,43 @@
+"""
+DEV:
+To run the test locally
+
+```sh
+export DJURA_METADATA_PATH=src/djura/record_selection/assets/NGA_W2_v2.pickle
+pytest -m slow
+```
+
+PowerShell (Windows):
+
+```powershell
+$env:DJURA_METADATA_PATH = `
+    "src/djura/record_selection/assets/NGA_W2_v2.pickle"
+pytest -m slow
+```
+
+CMD (Windows):
+
+```cmd
+set DJURA_METADATA_PATH=src/djura/record_selection/assets/NGA_W2_v2.pickle
+pytest -m slow
+```
+"""
+
 import pytest
 from pathlib import Path
-import numpy as np
 
-from src.gcim import GCIM
-from src.utilities import get_period_im
-from src.utilities import export_results
+from djura.record_selection.gcim import GCIM
 
 path = Path(__file__).resolve().parent
 
 
+@pytest.mark.slow
 class TestGCIM:
-    metadata = path.parents[0] / "src/assets/NGA_W2_v2.pickle"
 
     @pytest.mark.parametrize(
         "filename, conditional, data_create", [
             ("input1", True, False),
+            ("input2", None, False),
             ("input3", None, False),
             ("input1", None, True),
             ("input4", True, False),
@@ -40,20 +63,13 @@ class TestGCIM:
         else:
             class_data = input_file
 
-        gcim = GCIM(self.metadata, class_data, conditional=conditional)
+        gcim = GCIM(class_data, conditional=conditional)
 
         gcim.create(data=create_input)
-
-        if "im-star" in gcim.data:
-            imt = gcim.data['im-star']['type']
-            imn, _ = get_period_im(imt)
-
-            print(np.exp(gcim.output_create["target"]["mu_lnIMi"][imn]))
-            print(gcim.output_create['target']['IMi'][imn])
         gcim.select(data=create_input, output_create=gcim.output_create)
 
     def test_helpers(self):
-        gcim = GCIM(self.metadata)
+        gcim = GCIM()
 
         gcim.get_supported_rupture_parameters()
         gcim.get_supported_sites_parameters()
@@ -62,31 +78,3 @@ class TestGCIM:
         gcim.available_correlation_models()
         gcim.get_supported_im_component_types()
         gcim.get_available_gsims()
-
-    @pytest.mark.parametrize(
-        "id", [
-            (1),
-            # (2),
-            # (3),
-            # (4),
-            # (5),
-            # (6),
-            # (7),
-            # (8),
-            # (9),
-        ]
-    )
-    def test_temp(self, id):
-        tag = "conditioned_on_SA(0.5)_no_haz_cons"
-        input_file = path / f"applications/savvinos/{tag}/input1_IML{id}.json"
-
-        gcim = GCIM(self.metadata, conditional=True)
-
-        gcim.create(data=input_file)
-        import numpy as np
-        print(np.exp(gcim.output_create["target"]["mu_lnIMi"]['SA']))
-        print(gcim.output_create['target']['IMi']['SA'])
-        out = gcim.select(data=input_file, output_create=gcim.output_create)
-
-        export_results(
-            path / f"applications/savvinos/{tag}/outs/outs_{id}.json", out, "json")
