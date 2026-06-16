a10bf89e74d68b62f3879d278a13aa5e9802b948
Davit Shahnazaryan <davit.shahnazaryan@iusspavia.it>
2026-06-16 12:38:51 +0200

    tests: init tests for vertical component selection

---
+++ tests/rs/test_gcim_v.py
@@ -0,0 +1,39 @@
+"""
+DEV:
+To run the test locally
+
+```sh
+export DJURA_METADATA_PATH=src/djura/record_selection/assets/NGA_W2_v2.pickle
+pytest -m slow
+```
+
+PowerShell (Windows):
+
+```powershell
+$env:DJURA_METADATA_PATH = `
+    "src/djura/record_selection/assets/NGA_W2_v2.pickle"
+pytest -m slow
+```
+
+CMD (Windows):
+
+```cmd
+set DJURA_METADATA_PATH=src/djura/record_selection/assets/NGA_W2_v2.pickle
+pytest -m slow
+```
+"""
+
+import pytest
+from pathlib import Path
+
+from djura.record_selection.gcim import GCIM
+
+path = Path(__file__).resolve().parent
+
+@pytest.mark.slow
+class TestGCIMV:
+    def test_gcim_v(self):
+        input_file = path / "assets/gcim_inputs/input17.json"
+
+        gcim = GCIM(input_file, conditional=True)
+        gcim.create()
